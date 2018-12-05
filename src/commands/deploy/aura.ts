e90b5368f41942786e5a0ae58c281503044b6c77
Mohith Shrivastava <mohith@codescience.com>
2018-12-04 23:09:33 -0500

    first version aura deploy

--- src/commands/deploy/aura.ts
+++ src/commands/deploy/aura.ts
@@ -1,6 +1,7 @@
 import {core, flags, SfdxCommand} from '@salesforce/command';
 import chalk from 'chalk';
 import fs = require('fs-extra');
+import promisify = require('util');
 import {QueryResult} from '../../models/queryResult';
 import {SobjectResult} from '../../models/sObjectResult';
 
@@ -11,7 +12,7 @@ core.Messages.importMessagesDirectory(__dirname);
 // or any library that is using the messages framework can also be loaded this way.
 const messages = core.Messages.loadMessages('mo-dx-plugin', 'org');
 
-export default class TriggerDeploy extends SfdxCommand {
+export default class AuraDeploy extends SfdxCommand {
 
   public static description = messages.getMessage('commandDescription');
 
@@ -21,7 +22,6 @@ export default class TriggerDeploy extends SfdxCommand {
 
   protected static flagsConfig = {
     // flag with a value (-n, --name=VALUE)
-    classname: {type: 'string', required: false, char: 'n', description: 'name of the trigger' },
     filepath: {type: 'string', char: 'p', description: 'file path' }
   };
 
@@ -49,10 +49,47 @@ export default class TriggerDeploy extends SfdxCommand {
       Source: string;
     }
 
-    const filebody = await fs.readFile(this.flags.filepath, 'utf8');
-    const filefullName = this.flags.filepath.substring(this.flags.filepath.lastIndexof('/') + 1);
-    const fileName = filefullName.split('.')[0];
-    const fileExtension = filefullName.split('.')[1];
+    interface FileObject {
+      Name: string;
+      Body: string;
+    }
+
+    let isDirectory: boolean = false;
+    console.log(this.flags.filepath);
+    const temp = this.flags.filepath;
+    const index = temp.lastIndexOf('/');
+    console.log(index);
+    const pathString = temp.substring(index + 1);
+    console.log(pathString);
+    let fileNames;
+    let fileName;
+    let fileExtension;
+
+    if (pathString === pathString.split('.')[0]) {
+       isDirectory = true;
+    } else {
+      fileName = pathString.split('.')[0];
+      fileExtension = pathString.split('.')[1];
+    }
+
+    if (isDirectory) {
+      fileNames = await fs.readdir(this.flags.filepath);
+      const files = await getFileBodyMap(fileNames);
+      console.log(files);
+      console.log(fileNames);
+    }
+
+    // let fileName = filePathName.split('.')[0];
+   //  const fileExtension = filePathName.split('.')[1];
+
+   // function to get the file body concurrently using Promise.All
+    async function getFileBodyMap(files: string[]) {
+      return Promise.all(
+        files.map(async file => {
+          return await fs.readFile(temp + '/' + file , 'utf8');
+        })
+      );
+    }
 
     // function to get file extension
     function getDefType(extension: string, filename: string) {
@@ -81,11 +118,14 @@ export default class TriggerDeploy extends SfdxCommand {
         case 'js':
             filename = fileName.toLowerCase();
             if (filename.endsWith('controller')) {
+                fileName = fileName.replace('Controller', '');
                 return 'CONTROLLER';
             } else if (filename.endsWith('helper')) {
+                fileName = fileName.replace('Helper', '');
                 return 'HELPER';
             } else if (filename.endsWith('renderer')) {
                 // RENDERER — client-side renderer
+                fileName = fileName.replace('Renderer', '');
                 return 'RENDERER';
             }
             break;
@@ -96,6 +136,21 @@ export default class TriggerDeploy extends SfdxCommand {
       }
     }
 
-    return fileName;
+    // get Auraformar
+    function getAuraFormat(ext: string) {
+      // is 'js', 'css', or 'xml'
+      switch (ext) {
+          case 'js':
+              return 'JS';
+          case 'css':
+              return 'CSS';
+          default:
+              return 'XML';
+      }
+    }
+
+    this.ux.stopSpinner('Executed Successfully');
+
+    return '';
   }
 }
