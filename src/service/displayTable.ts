1398a92280844fb06f9043f7bae89b6b8c16560c
Mohith Shrivastava <mohith@codescience.com>
2019-02-16 09:37:14 -0500

    add some chalk

--- src/service/displayTable.ts
+++ src/service/displayTable.ts
@@ -1,4 +1,5 @@
 import {UX} from '@salesforce/command';
+import chalk from 'chalk';
 import { DeployResult } from '../service/deploy';
 import { CompileErrors } from '../types/errorLog';
 
@@ -6,16 +7,16 @@ export function display(deployResult: DeployResult, ux: UX) {
     const errors = [] as CompileErrors[];
     const tableColumnData = {
       columns: [
-        { key: 'lineNumber', label: 'Line' },
-        { key: 'columnNumber', label: 'Column' },
-        { key: 'problem', label: 'Error Description' }
+        { key: 'lineNumber', label: chalk.redBright.bold('Line')},
+        { key: 'columnNumber', label: chalk.redBright.bold('Column') },
+        { key: 'problem', label: chalk.redBright.bold('Error Description') }
       ]
     };
     for (const error of deployResult.queryResult.records[0].DeployDetails.componentFailures) {
       const errorLog = {} as CompileErrors;
       errorLog.columnNumber = error.columnNumber;
-      errorLog.lineNumber = error.lineNumber;
-      errorLog.problem = error.problem;
+      errorLog.lineNumber = (error.lineNumber);
+      errorLog.problem = chalk.bold(error.problem);
       errors.push(errorLog);
       // console.table(chalk.redBright('Line:' + error.lineNumber + ' Column:' + error.columnNumber + ' Error Description:' + error.problem + '\r\n'));
     }
