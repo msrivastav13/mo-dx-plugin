add1583a158315566b196ae56db6a683aa184474
Mohith Shrivastava <mohith@codescience.com>
2019-07-16 21:23:12 -0400

    Refactor

--- src/commands/generate/packagexml.ts
+++ src/commands/generate/packagexml.ts
@@ -1,17 +1,13 @@
 import { core, flags, SfdxCommand } from '@salesforce/command';
 import chalk from 'chalk';
-import * as child from 'child_process';
-import * as util from 'util';
-import {MetadataResult} from '../../models/metadataResult';
-import {PackageXmlTemplate} from '../../service/packagexmlTemplate';
+import {MetadataMember} from '../../service/packagexmlbuilder/metadataMember';
+import {PackageXmlTemplate} from '../../service/packagexmlbuilder/packagexmlTemplate';
 // Initialize Messages with the current plugin directory
 core.Messages.importMessagesDirectory(__dirname);
 
 // Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
 // or any library that is using the messages framework can also be loaded this way.
 const messages = core.Messages.loadMessages('mo-dx-plugin', 'org');
-
-const exec = util.promisify(child.exec);
 
 export default class PackageXML extends SfdxCommand {
 
@@ -35,38 +31,24 @@ export default class PackageXML extends SfdxCommand {
   protected static requiresUsername = true;
 
   public async run(): Promise<string> {
-    let packagexml ;
+    let packagexml: string ;
     const conn = this.org.getConnection();
     const apiVersion = conn.getApiVersion();
     const defaultusername = this.flags.targetusername
       ? this.flags.targetusername
       : this.org.getUsername();
     this.ux.startSpinner(chalk.yellowBright('Building package xml for ' + this.flags.metadatatype));
-    const listMetadata = `sfdx force:mdapi:listmetadata -m ${this.flags.metadatatype} -u ${defaultusername} --json`;
-    try {
-      const result =  await exec(listMetadata, {
-        maxBuffer: 1000000 * 1024
-      });
-      const jsonResult = result.stdout;
-      const metadatalist = JSON.parse(jsonResult).result as MetadataResult[];
-      packagexml = this.createXML(metadatalist, apiVersion);
-      process.stdout.write(packagexml);
-      this.ux.stopSpinner(chalk.greenBright('Package XML generated Successfully ✔'));
-    } catch (exception) {
-
-    }
+    packagexml = await this.createXML(this.flags.metadatatype, apiVersion, defaultusername);
+    process.stdout.write(packagexml);
+    this.ux.stopSpinner(chalk.greenBright('Package XML generated Successfully ✔'));
     return packagexml;
   }
 
-  private createXML(metadataResults: MetadataResult[], version: string): string {
+  private async createXML(metadatatype: string, version: string, defaultusername: string): Promise<string> {
     let xml = '' as string;
     xml = PackageXmlTemplate.createHeader();
-    xml = xml + PackageXmlTemplate.startType();
-    for (const componentname of metadataResults) {
-        xml = xml + PackageXmlTemplate.createMember(componentname.fullName);
-    }
-    xml = xml + PackageXmlTemplate.nameTag(this.flags.metadatatype);
-    xml = xml + PackageXmlTemplate.endType();
+    const metadataMember = new MetadataMember(metadatatype, defaultusername);
+    xml = xml + await metadataMember.createMemberXml();
     xml = xml + PackageXmlTemplate.createFooter(version);
     return xml;
   }
